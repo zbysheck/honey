7ec7b59d79fb851943f27166662448c2a1678897
Krystian Sakowski <krystian.sakowski@intel.com>
2015-01-24 01:44:23 +0100

    Added initial implementation of a husband who will chase for wife's lover.

--- code/platforms.py
+++ code/platforms.py
@@ -2,6 +2,7 @@
 Module for managing platforms.
 """
 import pygame
+import time
 
 from spritesheet_functions import SpriteSheet
 
@@ -99,3 +100,78 @@ class MovingPlatform(Platform):
         cur_pos = self.rect.x - self.level.world_shift
         if cur_pos < self.boundary_left or cur_pos > self.boundary_right:
             self.change_x *= -1
+
+class Husband(pygame.sprite.Sprite):
+    """ This class represents the bar at the bottom that the player
+    controls. """
+
+    # -- Attributes
+    # Set speed vector of player
+    change_x = 0
+    change_y = 0
+
+    # This holds all the images for the animated walk left/right of our player
+    walking_frames_l = []
+    walking_frames_r = []
+
+    # What direction is the player facing?
+    direction = "R"
+
+    def __init__(self):
+        super(Husband, self).__init__()
+
+        # Call the parent's constructor
+        pygame.sprite.Sprite.__init__(self)
+
+        sprite_sheet = SpriteSheet("p2_walk.png")
+        # Load all the right facing images into a list
+        image = sprite_sheet.get_image(0, 0, 66, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(66, 0, 66, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(132, 0, 67, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(0, 93, 66, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(66, 93, 66, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(132, 93, 72, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(0, 186, 70, 90)
+        self.walking_frames_r.append(image)
+
+        # Load all the right facing images, then flip them
+        # to face left.
+        image = sprite_sheet.get_image(0, 0, 66, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(66, 0, 66, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(132, 0, 67, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(0, 93, 66, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(66, 93, 66, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(132, 93, 72, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(0, 186, 70, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+
+        # Set the image the player starts with
+        self.image = self.walking_frames_r[0]
+
+        # Set a referance to the image rect.
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        print str(time.time()) + "\t" + "updating.."
+        self.rect.x = self.rect.x + 1
+        self.change_x = 0
+        self.change_y = 0
